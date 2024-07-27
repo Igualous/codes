9f37e79a8f111a3ad518c45c359d58103e1e7893
Igor Rodrigues <120427686+Igualous@users.noreply.github.com>
2024-07-26 22:50:45 -0300

    Update beecrowd1028.c
    código da certo, mas: "time limit exceeded"

--- beecrowd1028.c
+++ beecrowd1028.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-int tamanho_pilha[] = {};
-void calcular_MDC(int a, int b) {
+int calcular_MDC(int a, int b) {
    int j, i, tamanho_pilha;
    if (a > b) {
       for (j = b; j > 1; j--) { //tentando todos valores menores que o menor valor
          if (a%j == 0 && b%j == 0) { //se a divisao dos dois por j der inteiro
             tamanho_pilha = j;
-            printf("o tamanho da pilha é %d\n", tamanho_pilha);
             break;
          }
       }
@@ -14,13 +12,12 @@ void calcular_MDC(int a, int b) {
       for (j = a; j > 1; j--) {
          if (a%j == 0 && b%j == 0) {
             tamanho_pilha = j;
-            printf("o tamanho da pilha é %d\n", tamanho_pilha);
             break;
          }
       }
    }
+   return tamanho_pilha;
 }
-
 int main() {
    int N, F1, F2, i;
    scanf("%d", &N); //numero de casos de teste
@@ -29,17 +26,17 @@ int main() {
       printf("valor invalido\n");
       scanf("%d", &N);
    }
-  
+   int res[N];
    for (i = 0; i < N; i++) {
-     
       scanf("%d %d", &F1, &F2); //quantidade de figurinhas pra trocar
-     
       while (F1 < 1 || F1 > 1000 || F2 < 1 || F2 > 1000) {
          printf("valor invalido\n");
          scanf("%d %d", &F1, &F2);
          } 
-      calcular_MDC(F1, F2);
+      res[i] = calcular_MDC(F1, F2);
    }
-
+   for (i = 0; i < N; i++) {
+      printf("%d\n", res[i]);
+   }
    return 0;
 }
